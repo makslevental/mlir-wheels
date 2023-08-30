fd4c998c1b1c69f1008692ccd16198eec31ebf3d
max <maksim.levental@gmail.com>
2023-08-30 10:09:28 -0500

    backport to 35ca6498

--- scripts/apply_patches.sh
+++ scripts/apply_patches.sh
@@ -7,7 +7,6 @@ add_td_to_mlirpythoncapi_headers \
 mscv \
 register_test_pass \
 remove_openmp_dep_on_clang_and_export_async_symbols \
-tblgen \
 "
 
 if [[ x"${APPLY_PATCHES:-true}" == x"true" ]]; then
