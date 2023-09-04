2b6640cf21c8da410a7b1e8298844c2d80783287
Maksim Levental <maksim.levental@gmail.com>
2023-09-04 00:14:50 -0500

    Refactor (#14)
    * refactor
    
    * refactor
    
    * Update distro.yml
    
    * Update distro.yml
    
    * build arm64 native tools too
    
    * mlir wheel version

--- scripts/pip_install_mlir.sh
+++ scripts/pip_install_mlir.sh
@@ -5,11 +5,11 @@ export PIP_FIND_LINKS="https://github.com/makslevental/mlir-wheels/releases/expa
 
 SITE_PACKAGES=$(python -c "import site; print(site.getsitepackages()[0])")
 
-if [ ! -z "$MLIR_COMMIT" ]; then
-  pip install mlir-native-tools==$MLIR_COMMIT --force -U
+if [ ! -z "$MLIR_WHEEL_VERSION" ]; then
+  pip install mlir-native-tools==$MLIR_WHEEL_VERSION --force -U
 
-  DATE=$(echo $MLIR_COMMIT | cut -d "+" -f 1)
-  HASH=$(echo $MLIR_COMMIT | cut -d "+" -f 2)
+  DATE=$(echo $MLIR_WHEEL_VERSION | cut -d "+" -f 1)
+  HASH=$(echo $MLIR_WHEEL_VERSION | cut -d "+" -f 2)
   LOCAL_VERSION=""
   if [ x"$BUILD_CUDA" == x"true" ]; then
     LOCAL_VERSION="$LOCAL_VERSION cuda"
@@ -22,7 +22,7 @@ if [ ! -z "$MLIR_COMMIT" ]; then
   fi
   LOCAL_VERSION="$LOCAL_VERSION $HASH"
   LOCAL_VERSION=$(echo $LOCAL_VERSION | tr ' ' '.')
-  MLIR_COMMIT="==$DATE+$LOCAL_VERSION"
+  MLIR_WHEEL_VERSION="==$DATE+$LOCAL_VERSION"
 else
   pip install mlir-native-tools --force -U
 fi
@@ -33,7 +33,7 @@ if [ x"$CIBW_ARCHS" == x"arm64" ] || [ x"$CIBW_ARCHS" == x"aarch64" ]; then
   elif [ x"$MATRIX_OS" == x"ubuntu-20.04" ] && [ x"$CIBW_ARCHS" == x"aarch64" ]; then
     PLAT=linux_aarch64
   fi
-  pip install mlir$MLIR_COMMIT --platform $PLAT --only-binary=:all: --target $SITE_PACKAGES --no-deps --force -U
+  pip install mlir$MLIR_WHEEL_VERSION --platform $PLAT --only-binary=:all: --target $SITE_PACKAGES --no-deps --force -U
 else
-  pip install mlir$MLIR_COMMIT --force -U
+  pip install mlir$MLIR_WHEEL_VERSION --force -U
 fi
