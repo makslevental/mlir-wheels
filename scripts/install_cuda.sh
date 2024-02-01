c8dff8c6919f1a851d6cc4d5ad2c9ad57f37d99a
max <maksim.levental@gmail.com>
2024-02-01 17:05:29 -0600

    maybe fix cuda?

--- scripts/install_cuda.sh
+++ scripts/install_cuda.sh
@@ -3,7 +3,7 @@ set -xe
 
 if [[ x"${BUILD_CUDA}" == x"true" ]]; then
   wget -q https://developer.download.nvidia.com/compute/cuda/12.2.0/local_installers/cuda_12.2.0_535.54.03_linux.run
-  sh cuda_12.2.0_535.54.03_linux.run --toolkit --silent --override
+  sh cuda_12.2.0_535.54.03_linux.run --toolkit --override --silent
   if [[ $? -ne 0 ]]; then
     echo "CUDA Installation Error."
     exit 1
@@ -15,6 +15,5 @@ if [[ x"${BUILD_CUDA}" == x"true" ]]; then
   echo "CUDA_PATH=${CUDA_PATH}"
   export CUDA_PATH=${CUDA_PATH}
   export PATH="$CUDA_PATH/bin:$PATH"
-  export LD_LIBRARY_PATH="$CUDA_PATH/lib:$LD_LIBRARY_PATH"
-  export LD_LIBRARY_PATH="$CUDA_PATH/lib64:$LD_LIBRARY_PATH"
+  export LD_LIBRARY_PATH="$CUDA_PATH/targets/x86_64-linux/lib/stubs:$LD_LIBRARY_PATH"
 fi
