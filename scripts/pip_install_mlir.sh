78c8c2678eeaf527e86a1aff7365c70b0cbd008a
Maksim Levental <maksim.levental@gmail.com>
2025-02-01 10:12:17 -0500

    Update pip_install_mlir.sh

--- scripts/pip_install_mlir.sh
+++ scripts/pip_install_mlir.sh
@@ -24,7 +24,7 @@ else
 fi
 
 if [ x"$CIBW_ARCHS" == x"arm64" ] || [ x"$CIBW_ARCHS" == x"aarch64" ]; then
-  if [ x"$MATRIX_OS" == x"macos-13" ] || [ x"$MATRIX_OS" == x"macos-13" ]; then
+  if [ x"$MATRIX_OS" == x"macos-13" ] || [ x"$MATRIX_OS" == x"macos-14" ]; then
     PLAT=macosx_12_0_arm64
   elif [ x"$MATRIX_OS" == x"ubuntu-20.04" ] || [ x"$MATRIX_OS" == x"ubuntu-22.04-arm" ]; then
     PLAT=linux_aarch64
